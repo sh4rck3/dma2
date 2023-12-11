3d3f44447585a5052d00444fc26fa894399a4e61
joão JS. santos <hd-071@dunice.local>
2023-12-11 17:23:53 -0300

    Página de controle de o SMS

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -120,6 +120,47 @@ const pageRole = computed(() => page.props.user.roles)
                                             </DropdownLink>
                                         </div>
                                     </template>
+                                    
+                                </Dropdown>
+                            </div>
+                            <div 
+                                v-if="pageRole.includes('admin') || pageRole.includes('suporte')"
+                                class="hidden space-x-8 sm:ml-10 sm:flex mt-4">
+                                <Dropdown align="right" width="60" :active="route().current('useradm')">
+                                    <template #trigger>
+                                        <span class="inline-flex rounded-md">
+                                            <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none focus:bg-gray-50 dark:focus:bg-gray-700 active:bg-gray-50 dark:active:bg-gray-700 transition ease-in-out duration-150">
+                                                Adm. SMS
+
+                                                <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 15L12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
+                                                </svg>
+                                            </button>
+                                        </span>
+                                    </template>
+
+                                    <template #content>
+                                        <div class="w-60">
+                                            <!-- user Management -->
+                                            <div class="block px-4 py-2 text-xs text-gray-400">
+                                                Controle de SMS
+                                            </div>
+
+                                            <!-- Team Settings -->
+                                            <DropdownLink>
+                                                Envio de SMS único
+                                            </DropdownLink>
+
+                                            <DropdownLink>
+                                                Envio de SMS em massa
+                                            </DropdownLink>
+
+                                            <DropdownLink :href="route('smsadmin')">
+                                                Lista de SMS
+                                            </DropdownLink>
+                                        </div>
+                                    </template>
+                                    
                                 </Dropdown>
                             </div>
                             <!-- user Dropdown meeting -->
